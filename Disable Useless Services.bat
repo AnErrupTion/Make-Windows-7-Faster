511223304d4ff3b6760c1b99b07c0e165f1a2655
ShiningLea <49339966+AnErrupTion@users.noreply.github.com>
2019-12-23 18:33:22 +0100

    1.1: Fix config start

--- Disable Useless Services.bat
+++ Disable Useless Services.bat
@@ -1,11 +1,11 @@
 @echo off
 title [Disable Useless Services by AnErrupTion]
-sc stop RemoteRegistry & sc config RemoteRegistry start=disabled
-sc stop DPS & sc config DPS start=demand
-sc stop CscService & sc config CscService start=demand
-sc stop Spooler & sc config Spooler start=demand
-sc stop seclogon & sc config seclogon start=demand
-sc stop wscsvc & sc config wscsvc start=demand
-sc stop lmhosts & sc config lmhosts start=demand
-sc stop WerSvc & sc config WerSvc start=demand
-sc stop WSearch & sc config WSearch start=demand
+sc stop RemoteRegistry & sc config RemoteRegistry start= disabled
+sc stop DPS & sc config DPS start= demand
+sc stop CscService & sc config CscService start= demand
+sc stop Spooler & sc config Spooler start= demand
+sc stop seclogon & sc config seclogon start= demand
+sc stop wscsvc & sc config wscsvc start= demand
+sc stop lmhosts & sc config lmhosts start= demand
+sc stop WerSvc & sc config WerSvc start= demand
+sc stop WSearch & sc config WSearch start= demand
